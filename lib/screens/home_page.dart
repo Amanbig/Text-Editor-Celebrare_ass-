b14cc8770c6280cc1b45dbe39677da9aedc8a220
Amanpreet Singh <amanpreetsinghjhiwant@outlook.com>
2024-12-21 12:46:07 +0530

    Updated main.dart file

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart'; // Move this to the top
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
